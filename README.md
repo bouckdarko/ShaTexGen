aaa6b40bf78eff9221d2273784df6936a047974f
Bfhrc7 <bouck.darko@gmail.com>
2024-10-11 17:33:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ShaTexGen
+
+This is the README file for ShaTexGen.
+
+## Installation
+
+Describe how to install ShaTexGen.
+
+## Usage
+
+Describe how to use ShaTexGen.
---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+"""
+Test module for main.py.
+"""
+import unittest
+from main import main
+
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        self.assertIsNone(main())
+
+if __name__ == "__main__":
+    unittest.main()
